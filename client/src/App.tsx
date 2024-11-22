515c72e0178a69f59fafad857f8c62349f889389
Tóth Dániel <tothdani561@gmail.com>
2024-11-22 18:20:30 +0100

    Add basic homePage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
 import './App.css';
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Simple React Typescript Tailwind Sample
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );  
 }
