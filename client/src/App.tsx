68148c0a529e26c7914e6529ae3079262e4ba252
Tóth Dániel <tothdani561@gmail.com>
2024-11-22 17:39:33 +0100

    React app init

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-600">
+      Simple React Typescript Tailwind Sample
+    </h1>
+  );  
+}
